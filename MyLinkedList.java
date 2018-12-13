42ffe2ca0b79c24c1afaedf97ca737b5066f1068
dramsammy <dramsammy@stuy.edu>
2018-12-13 11:41:16 -0500

    Added constructors for the classes

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -2,9 +2,15 @@ public class MyLinkedList{
   private class Node{
     private int data;
     private Node next,prev;
+    // Empty Constructor
+    public Node(){
+    }
   }
   private int size;
   private Node start,end;
+  // Empty Constructor
+  public MyLinkedList(){
+  }
   // Returns the size of the MyLinkedList
   public int Size(){
     return size;
