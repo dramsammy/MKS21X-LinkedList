baffabd35831aa1753559127e5c69957bd791b0c
dramsammy <dramsammy@stuy.edu>
2018-12-13 11:30:27 -0500

    Finished size method

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,5 +1,11 @@
 public class MyLinkedList{
   private class Node{
-    
+
+  }
+  private int size;
+  private Node start,end;
+  // Returns the size of the MyLinkedList
+  public int Size(){
+    return size;
   }
 }
